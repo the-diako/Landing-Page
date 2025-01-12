38896d4901714358ad843ecbd8fc6e1c1cb4d857
jacky boy <hello.1234.1994@gmail.com>
2025-01-12 17:44:23 +0330

    Added the close and menu icon to the assets folder.

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,38 +1,40 @@
-import apps from "../assets/apps-image.png";
-import bg from "../assets/bg.png";
-import blueLogo from "../assets/blue-logo.png";
-import WhiteLogo from "../assets/white-logo.png";
-import WhiteQuote from "../assets/white-quote.png";
-import blueQuote from "../assets/blue-quote.png";
-import clients01 from "../assets/clients01.png";
-import clients02 from "../assets/clients02.png";
-import clients03 from "../assets/clients03.png";
-import clients04 from "../assets/clients04.png";
-import customize01 from "../assets/customize01.png";
-import customize02 from "../assets/customize02.png";
-import data from "../assets/data-image.png";
-import facebook from "../assets/facebook.png";
-import twitter from "../assets/twitter.png";
-import linkedin from "../assets/Linkedin.png";
-import hero from "../assets/hero.png";
-import android from "../assets/android.png";
-import ios from "../assets/ios.png";
-import windows from "../assets/windows.png";
-import planBlackIcon from "../assets/plan-black-icon.png";
-import planYellowIcon from "../assets/plan-yellow-icon.png";
-import sideBg01 from "../assets/side-bg01.png";
-import sideBg02 from "../assets/side-bg02.png";
-import textBgBlue from "../assets/text-bg-blue.png";
-import textBgYellow from "../assets/text-bg-yellow.png";
-import user01 from "../assets/user01.png";
-import user02 from "../assets/user02.png";
-import user03 from "../assets/user03.png";
-import workBg from "../assets/work-bg.png";
-import workManagemnent01 from "../assets/work-management01.png";
-import workManagemnent02 from "../assets/work-management02.png";
-import downArrow from "../assets/downArrow.png";
-import rightArrow from "../assets/rightArrow.png";
-import web from "../assets/web.png";
+import apps from "./apps-image.png";
+import bg from "./bg.png";
+import blueLogo from "./blue-logo.png";
+import WhiteLogo from "./white-logo.png";
+import WhiteQuote from "./white-quote.png";
+import blueQuote from "./blue-quote.png";
+import clients01 from "./clients01.png";
+import clients02 from "./clients02.png";
+import clients03 from "./clients03.png";
+import clients04 from "./clients04.png";
+import customize01 from "./customize01.png";
+import customize02 from "./customize02.png";
+import data from "./data-image.png";
+import facebook from "./facebook.png";
+import twitter from "./twitter.png";
+import linkedin from "./Linkedin.png";
+import hero from "./hero.png";
+import android from "./android.png";
+import ios from "./ios.png";
+import windows from "./windows.png";
+import planBlackIcon from "./plan-black-icon.png";
+import planYellowIcon from "./plan-yellow-icon.png";
+import sideBg01 from "./side-bg01.png";
+import sideBg02 from "./side-bg02.png";
+import textBgBlue from "./text-bg-blue.png";
+import textBgYellow from "./text-bg-yellow.png";
+import user01 from "./user01.png";
+import user02 from "./user02.png";
+import user03 from "./user03.png";
+import workBg from "./work-bg.png";
+import workManagemnent01 from "./work-management01.png";
+import workManagemnent02 from "./work-management02.png";
+import downArrow from "./downArrow.png";
+import rightArrow from "./rightArrow.png";
+import web from "./web.png";
+import close from "./close.svg";
+import menu from "./menu.svg";
 
 export {
   apps,
@@ -70,4 +72,6 @@ export {
   rightArrow,
   downArrow,
   web,
+  close,
+  menu,
 };
