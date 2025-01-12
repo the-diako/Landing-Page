c7a214b19977b550a11983ce06c8420808bd686d
jacky boy <hello.1234.1994@gmail.com>
2025-01-12 17:31:31 +0330

    Complete initial setup of the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import WorkManagement from "./components/WorkManagement";
+import Extension from "./components/Extension";
+import Customize from "./components/Customize";
+import Pricing from "./components/Pricing";
+import Work from "./components/Work";
+import Data from "./components/Data";
+import Sponsors from "./components/Sponsors";
+import Apps from "./components/Apps";
+import Testimonials from "./components/Testimonials";
+import FreeTrial from "./components/FreeTrial";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="w-full py-2">
+      <Navbar />
+
+      <Hero />
+      <WorkManagement />
+      <Extension />
+      <Customize />
+      <Pricing />
+      <Work />
+      <Data />
+      <Sponsors />
+      <Apps />
+      <Testimonials />
+      <FreeTrial />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
