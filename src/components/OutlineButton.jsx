275c1a2a9c2186fd5cb43e900582c725ad141873
jacky boy <hello.1234.1994@gmail.com>
2025-01-15 02:16:23 +0330

    Updated the text color.

--- src/components/OutlineButton.jsx
+++ src/components/OutlineButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const OutlineButton = ({ title, style }) => {
   return (
     <button
-      className={`bg-primary-light text-white px-5 py-2 rounded-md outline-none font-Inter ${style}`}
+      className={`bg-primary-light text-black px-5 py-2 rounded-md outline-none font-Inter ${style}`}
     >
       {title}
     </button>
